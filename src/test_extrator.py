a23376e2c8b80c933fafb1fc68caa140d98baa70
Ayanda <Khanyile_Ayanda@proton.me>
2026-04-04 02:53:37 +0200

    F: wrote and tested the extrator functions that extract markdown images and links from text

---
+++ src/test_extrator.py
@@ -0,0 +1,16 @@
+import unittest
+
+from extrator import extract_markdown_images, extract_markdown_links
+
+class TestExtrator(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        matches = extract_markdown_images(text)
+        answers = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(matches, answers)
+        
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_links(text)
+        answers = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(matches, answers)
